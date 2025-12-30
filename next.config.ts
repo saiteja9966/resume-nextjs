73487dfa81d06e9c8c5f21a7b3ae18173f9fee76
saiteja <tech.dev1@varunmotors.co.in>
2025-12-30 18:04:18 +0530

    fdsfdsf

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-//   output: "export",
-  images: {
+  output: "export",
+  basePath: "/resume-nextjs",        // 👈 YOUR REPO NAME
+  assetPrefix: "/resume-nextjs/",    
+   images: {
     unoptimized: true,
-  },
+  },// 👈 SAME REPO NAME
 };
 
 export default nextConfig;
